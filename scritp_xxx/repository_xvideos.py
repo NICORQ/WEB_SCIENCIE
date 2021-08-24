376e2a298a55c6601ea81100f5c3dcc262940fd3
bencenodev <robsoncol1947@gmail.com>
2021-08-23 20:18:13 -0500

    change

--- scritp_xxx/repository_xvideos.py
+++ scritp_xxx/repository_xvideos.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-def insertValues( id , title , href , name_site ):
+def insertValues( id , title , tags ,href , name_site ):
     con = sqlite3.connect('/home/bencenodev/entregable/tags_porn_xvideos')
     cursor = con.cursor()
-    sql = '''INSERT INTO 'tags_xvideos'( 'ID' , 'TITLE', 'HREF', 'NAME_SITE') VALUES(?,?,?,?)'''
+    sql = '''INSERT INTO 'tags_xvideos'( 'ID' , 'TAG' , 'TITLE', 'HREF', 'NAME_SITE') VALUES(?,?,?,?)'''
 
-    cursor.execute(sql, (id, title, href , name_site) )
+    cursor.execute(sql, (id, title, tags, href, name_site) )
     con.commit()
     cursor.close()
--- scritp_xxx/spiders/obt_data_xxx.py
+++ scritp_xxx/spiders/obt_data_xxx.py
@@ -1,32 +1,42 @@
 import scrapy
 from scritp_xxx.repository_xvideos import insertValues
-
+from scritp_xxx.spiders.obt_tags import ObtTags
+from scrapy.crawler import CrawlerProcess
 
 class ObtData(scrapy.Spider):
     name = "obtdata"
 
     def start_requests(self):
-        a: int = 1
-        for a in range(1, 20000):
-            url: str = f"https://www.xvideos.com/new/{a}"
 
+        for a in range(1,10):
+            url: str = f"https://www.xvideos.com/new/{a}"
             yield scrapy.Request(url=url, callback=self.parse)
 
 
-    # def parse(self, response):
-    #    page = response.url.split("/")[-1]
-    #   filename = 'xvideos-%s.html' % page
-    #  with open(filename, 'wb') as f:
-    #     f.write(response.body)
-    # self.log('Saved file %s' % filename)
+
+
 
     def parse(self, response):
 
         for xvideo in response.css('body'):
+
             title = xvideo.css('p.title a::attr(title)').getall()
             href = xvideo.css('p.title a::attr(href)').getall()
 
-            for t, h in zip(title,href):
+            for k,h in zip(title,href):
+                k: str = k.lower()
+                if ("venezuela" in k) or ("veneca" in k) or ("venezolana" in k):
+                    hre: str = "https://www.xvideos.com"+h
 
-                insertValues(None, t, "https://www.xvideos.com"+h, "xvideos")
+
+                    tagsRaw = ObtTags.obtHref(self, hre)
+
+                    for d in tagsRaw:
+                        print( "colombia" + str(d))
+
+
+                    for t, h in zip(title, href):
+                        insertValues(None, t, tags, "https://www.xvideos.com"+h, "xvideos" )
+
+
 
